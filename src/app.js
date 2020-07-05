f76e6a2b6e787c4a843213df25c005056923e091
Dipesh Basnet <bdipesh08@gmail.com>
2020-07-05 14:01:56 +0545

    data base connection and require module configure.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+const cors =  require('cors')
 
 
-var app = express();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+app.use('/static', express.static('uploads'));
+app.use(express.static(path.join(__dirname, '../uploads')));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-
+// app.use('/api/v1/root', swaggerUi.serve, swaggerUi.setup(swaggerDocument
+// app.use('/api/v1/users', userRoutes);
+// app.use('/api/v1/batch', batchRoutes);
+// app.use('/api/v1/course', courseRoutes);
+// app.use('/api/v1/notice', noticeRoutes);
+// app.use('/api/v1/attendance', attendanceRoutes);
 
 module.exports = app;
