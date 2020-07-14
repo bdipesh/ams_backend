f59f9c80d7b6dac13afb772e0a8dda21b7ec408c
lizanbajracharya <lizan.bajra97@gmail.com>
2020-07-14 14:19:32 +0545

    notice route update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const courseRoutes = require('../src/routes/course')
 const batchRoutes = require('../src/routes/batch')
 const userRoutes = require('../src/routes/users')
 const attendanceRoutes = require('../src/routes/attendance')
-const noticeRoutes = require('../src/routes/notice')
+// const noticeRoutes = require('../src/routes/notice')
 
 const app = express();
 app.use(cors());
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '../uploads')));
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/batch', batchRoutes);
 app.use('/api/v1/course', courseRoutes);
-app.use('/api/v1/notice', noticeRoutes);
+// app.use('/api/v1/notice', noticeRoutes);
 app.use('/api/v1/attendance', attendanceRoutes);
 
 module.exports = app;
--- src/routes/notice.js
+++ src/routes/notice.js
@@ -35,4 +35,4 @@ router.post('/',
 
     });
 
-export default router;
+module.exports = router;
